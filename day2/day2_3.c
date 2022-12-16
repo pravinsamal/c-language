fae357d12ca12967218523da6a7e33a91ecfc0e5
pravinsamal <samalpravin0406@gmail.com>
2022-12-17 00:37:19 +0530

    added

---
+++ day2/day2_3.c
@@ -0,0 +1,35 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void main () {
+
+    int val=50;
+
+    float f=40.67;
+    char ch ='a';
+
+    printf("value of val : %d ,value of f %f ,value of character %c",val,f,ch);
+}
+
+
+
+
+
+
+
+
+
+// int main (void ) {
+
+
+
+
+// int a =30;
+
+// printf("value of a %d",a);
+
+//     return 0;
+// }
